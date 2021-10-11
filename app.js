57867ddf78eaba8bbc38e685df02be494df31093
Sergio Rodriguez <sergio.rolo94@gmail.com>
2021-10-11 14:58:54 +0200

    más cambios

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-// QUERY SELETOR
 const display = document.querySelector(".display");
-// FETCH
+
 const getPokemon = async (pokemon) => {
   let result = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=150`);
   let data = await result.json();
-  //MAPEO DATA
+
   const pokemons = data.results.map((element, img) => ({
     name: element.name,
     image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
@@ -14,12 +13,12 @@ const getPokemon = async (pokemon) => {
 
   displayPokemons(pokemons);
 };
-// DISPLAY POKEMONS
+
 const displayPokemons = (pokemons) => {
   const pokemonHTML = pokemons
     .map(
       (pokemon) =>
-        `<li class="display__element">
+        `<li>
         <h2>${pokemon.name}</h2>
         <img src="${pokemon.image}" alt="${pokemon.name}"/>
         </li>`
