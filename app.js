9e4c888796f56392898f1258c21e9bf78d751325
Sergio Rodriguez <sergio.rolo94@gmail.com>
2021-10-12 20:15:10 +0200

    cambio forma de obtener las imagenes

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const display = document.querySelector(".display");
+window.onload = () => { // AQUI LO QUE HAGO ES DECIRLE AL NAVEGADOR QUE ME REALICE LA FUNCION CUANDO SE CARGUE LA PAGINA
+  getPokemons()
+}
 
-const getPokemon = async (pokemon) => {
-  let result = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=150`);
-  let data = await result.json();
+const getPokemons = async() => { // AQUI OBTENEMOS LOS POKEMONS
+  let pokemons = [] // CREO UN ARRAY DONDE IRAN METIDOS TODOS LOS POKEMONS
+  for (let i = 1; i < 151; i++) { // MEDIANTE UN FOR, CONSEGUIMOS QUE ACCEDA SOLO A LOS 150 PRIMEROS
+    let pokemonURL = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`) // ATACO AL ENDPOINT DE LA APPI PARA CONSEGUIR LOS POKEMONS
+    let pokemonJson = await pokemonURL.json() // CONVIERTO EN FORMATO JSON 
+    pokemons.push(pokemonJson) // METO TODOS LOS POKEMONS EN EL ARRAY "POKEMONS"
+  }
+  let pokemonsMap = pokemons.map(pokemon => (  // HAGO MAP PARA ACCEDER A LO QUE NECESITO, EN ESTE CASO A LA IMAGEN 
+    {
+      name: pokemon.forms[0].name, // BUCEO HASTA QUE OBTENGO EL NOMBRE
+      imagen: pokemon.sprites["front_default"] // BUCEO HASTA QUE OBTENGO LA IMAGEN
+    }
+  ))
+  drawPokemons(pokemonsMap) 
+}
 
-  const pokemons = data.results.map((element, img) => ({
-    name: element.name,
-    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-      img + 1
-    }.png`,
-  }));
-
-  displayPokemons(pokemons);
-};
-
-const displayPokemons = (pokemons) => {
-  const pokemonHTML = pokemons
-    .map(
-      (pokemon) =>
-        `<li>
-        <h2>${pokemon.name}</h2>
-        <img src="${pokemon.image}" alt="${pokemon.name}"/>
-        </li>`
-    )
-    .join("");
-  display.innerHTML = pokemonHTML;
-};
-
-getPokemon();
+ const drawPokemons = (pokemons) => { // AQUI ME METO EN EL UL "DISPLAY" DONDE VAN A IR MIS POKEMONS. LUEGO DIBUJO CADA UNO, QUE SERA UN LI, CON SU RESPECTIVO NOMBRE E IMAGEN.
+   let ul = document.querySelector(".display")
+   let mapeo = pokemons.map(pokemon =>
+    `<li>
+    <h2>${pokemon.name}</h2>
+    <img src="${pokemon.imagen}"
+    </li>`
+    )  
+    ul.innerHTML = mapeo // HAGO INNER HTML DE MAPEO PARA INTRODUCIR LO QUE HE HECHO EN MI HTML,
+ }
